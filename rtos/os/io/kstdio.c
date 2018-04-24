ef8deaab00b362053c2dba3f7cc4136f6464acaa
Juan Manuel Torres Palma <j.m.torrespalma@gmail.com>
2018-04-24 19:32:12 +0900

    [add] kstdio.h: library functions for i/o
    Implements kgets() and kputs() to easily handle
    stdin and stdout with the standard uart.

---
+++ rtos/os/io/kstdio.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2018 Juan Manuel Torres Palma <j.m.torrespalma@gmail.com>
+ * This file is part of ucmaze.
+ *
+ * ucmaze is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ucmaze is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ucmaze.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <kstdio.h>
+#include <uart.h>
+
+extern struct uart_dev uart_std;
+
+char *kgets(char *str)
+{
+	uint8_t r;
+	int rv;
+
+	do {
+		rv = uart_read_byte(&uart_std, &r);
+		*str++ = (char)r;
+	} while (r != '\n' && !rv);
+
+	if (rv)
+		return NULL;
+
+	return str;
+}
+
+int kputs(const char *str)
+{
+	uint8_t r;
+	int rv;
+
+	do {
+		r = (uint8_t)*str++;
+		rv = uart_send_byte(&uart_std, r);
+	} while (r != '\0' && !rv);
+
+	if (rv)
+		return EOF;
+
+	rv = uart_send_byte(&uart_std, '\n');
+	if (rv)
+		return EOF;
+
+	return 0;
+}
